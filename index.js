1d53c91a7ed109d308924c178bdf5bc1c3a65867
Foexna Dev Account <me@ashisharma.info>
2025-02-15 13:31:55 +0530

    Port updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/',(req, res) => {
     res.send('This is FXN Corp Demo Backend for azure, running on Port: '+ port)
