1a23955317505792c3f268cb279fffba9f060fb7
Foexna Dev Account <me@ashisharma.info>
2025-02-15 13:21:32 +0530

    Add initial type definitions and module exports for various packages

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',(req, res) => {
+    res.send('This is FXN Corp Demo Backend for azure, running on Port: '+ port)
+})
+
+app.listen(port, () =>{
+    console.log('This is FXN Corp Demo Backend for azure, running on Port: '+ port)
+})
